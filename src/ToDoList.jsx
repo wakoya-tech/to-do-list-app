35a7f1e56f2da42d8c15afa25633c3bd1da5f7ad
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2026-01-17 20:37:27 +0300

    Update ToDoList.jsx

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -11,7 +11,6 @@ function ToDoList() {
       setNewTask("");
     }
   }  
-  
   function deleteTask(index) {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
